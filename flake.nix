8d65cefb04d67ad9ff630c118f90954726dcd65f
oscar370 <oscar.dga@outlook.com>
2026-02-14 21:48:44 -0600

    added flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Node.js and pnpm development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.pnpm
+          ];
+        };
+      });
+}
